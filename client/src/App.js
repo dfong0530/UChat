f66c9f602a5caf2d487b70c13faf98739d060804
David Fong <dfong633@gmail.com>
2022-08-07 18:14:47 -0400

    react-router and context api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Message from './Pages/Messages.js'
+import Initial from "./Pages/Initial";
+import Login from "./Pages/Login";
+import CreateAccount from "./Pages/CreateAccount";
+import {useState} from "react";
+import GlobalContext from './GlobalContext';
+import {Routes, Route, useNavigate} from "react-router"
 
 function App() {
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null); //{_id, username, password, friends: [{ roomID: string, friendUsername: string}]}
+  const [room, setRoom] = useState({roomID: "", room: "", messages: []}); //{roomID: str, room: str, messages: [{_id: str, userID, message: str}]}
+
   return (
     <>
-      <Message/>
+      <GlobalContext.Provider value={{navigate, user, setUser,room, setRoom}}>
+        <Routes>
+          <Route path="/" element={<Initial />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/messages" element={<Message />} />
+        </Routes>
+      </GlobalContext.Provider>
     </>
   );
 }
---
+++ client/src/Data/GetData.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const Login = async(username, password) => {
+
+}
+
+
+export const CreateAccount = async() => {
+
+}
+
+export const GetRoomData = async() => {
+
+}
