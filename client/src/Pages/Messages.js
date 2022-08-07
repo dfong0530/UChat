666bd791ec9efc6f33ffa888e384279648fcb241
Justin Tran <jus81818@yahoo.com>
2022-08-07 18:23:09 -0400

    more changes

--- client/src/Pages/Messages.js
+++ client/src/Pages/Messages.js
@@ -1,4 +1,5 @@
 import "./CSS/Messages.css";
+import addIcon from "./images/add-friend-icon.jpg";
 
 const Messages = () => {
     return (
@@ -9,12 +10,12 @@ const Messages = () => {
                         <p>
                             UChat
                         </p>
-
+                        
+                        <img src={addIcon} />
                         
                     </div>
 
                     <div className='friends'>
-
                     </div>
                 </div>
 
