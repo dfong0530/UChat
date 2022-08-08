855d8cb3d1c94bce30e16d7c7d0adda4dcc0bb9c
Justin Tran <jus81818@yahoo.com>
2022-08-08 16:27:41 -0400

    login page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -3,7 +3,31 @@ import "./CSS/Login.css";
 const Login = () => {
     return (
         <>
-           
+            <section className="login-page">
+                <p className="title">
+                    UChat
+                </p>
+
+                <form className="info">
+                    <input 
+                        className="name"
+                        placeholder="Username"
+                    />
+                    
+                    <input 
+                        className="pass"
+                        placeholder="Password"
+                    />   
+
+                    <button className="login-button">
+                        Login
+                    </button>
+                </form>
+
+                <p className="create-account">
+                    or <a href="#">create an account</a> 
+                </p>
+            </section>
         </>
     );
 }
