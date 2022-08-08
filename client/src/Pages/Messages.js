36db9c9c7c0225ff31f2335a25b5f9fee6a06a70
Justin Tran <jus81818@yahoo.com>
2022-08-08 15:06:49 -0400

    more changes to message

--- client/src/Pages/Messages.js
+++ client/src/Pages/Messages.js
@@ -20,38 +20,74 @@ const Messages = () => {
 
     return (
         <> 
-            <section className='page'>
-                <div className='side'>
-                    <div className='add'>
+            {/* a css grid that represents the whole page */}
+            <section className="page">
+                {/* the side part of the page that displays
+                 the add friend button along with the friends */}
+                <div className="sidebar">
+                    {/* the add feature of the side */}
+                    <div className="add-friend">
+                        {/* the app name */}
                         <p>
                             UChat
                         </p>
-                        
+                        {/* the add feature */}
                         <img src={addIcon} />
-                        
                     </div>
 
-                    <div className='friends'>
+                    {/* the friends feature of the side */}
+                    <div className="friends">
+                        {/* there is a friend with a profile pic and their name */}
                         <div className="single-friend">
-                            <div className="profile-pic">
-                                
-                            </div>
-
+                            <div className="profile-pic"></div>
                             <p>
-                                Sarah 
+                                Sarah
                             </p> 
                         </div>
-
-                        <div className="single-friend">
-                            <p>
-                                Veevek
-                            </p>
-                        </div> 
                     </div>
                 </div>
 
-                <div className='main'>
+
+                {/* now for the main part of the messages page 
+                that includes the head, the chat UI, and the 
+                messages input feature */}
+                <div className="main">
+                    <div className="header">
+                        {/* the information regarding the friend w/
+                        their profile picture, where they are from 
+                        and their name */}
+                        <section className="info">
+                            {/* the profile picture */}
+                            <div className="profile-pic"></div>
+                            
+                            {/* for the name and location */}
+                            <div className="name-location">
+                                <p className="name">
+                                    Veevek
+                                </p>
+
+                                <p className="location">
+                                    From Kyiv
+                                </p>
+                            </div>
+                        </section>
+                        
+                        {/* this is the donate button */}
+                        <button className="donate"> 
+                            Donate Now
+                        </button>
+                    </div>
+
+
+
+                    {/* the CHAT PART */}
+                    <div className="messages-chat"></div>
                     
+
+                    {/* THE INPUT PART WHERE YOU COLLECT THE DATA AND 
+                    MANIPULATE IT*/}
+                    <div className="message-input">
+                    </div>
                 </div>
             </section>
         </>
