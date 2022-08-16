1e63c3d04faea7ae9f6e70254a26617e28ebf5be
David Fong <dfong633@gmail.com>
2022-08-15 23:27:38 -0400

    justin

--- client/src/JustinComponents/Friends.js
+++ client/src/JustinComponents/Friends.js
@@ -3,15 +3,6 @@ import PersonIcon from '@mui/icons-material/Person';
 
 
 const Friends = ({roomID, name}) => {
-<<<<<<< HEAD
-
-    const handleSwitch = () => {
-
-    }
-
-
-=======
->>>>>>> 96fa6bfcfd1ddb8ef1b18577fa581f914cf6f305
     return (
         <div className="single-friend">
                 <div className="profile-pic">
