49739d3513f8e2f2a722588c5af5546cab1067c2
David Fong <dfong633@gmail.com>
2022-08-16 22:59:35 -0400

    bugs

--- client/src/JustinComponents/Friends.js
+++ client/src/JustinComponents/Friends.js
@@ -1,33 +1,37 @@
 import "./CSS/Friends.css";
 import PersonIcon from '@mui/icons-material/Person';
-
-const handleFriend = ({socket, user}) => {
-    socket.emit('join-room', {_id: user._id, name: user.name, 
-    inUkraine: user.inUkraine}); 
-};
-
-const handleSwitch = async({socket, aFriend, user, room, GetRoomData}) => {
-    aFriend.roomID === room.roomID ? "light" : "regular";
-    const ret = await GetRoomData(aFriend.roomID, user.username, user.password);
-    socket.emit('leave-room', room.room);
-    socket.emit('switch-room', ret.room); 
-    setRoom(ret); 
-};
-
-//When your calling this function make sure that thre is more than one
-//friend. --> Could cause bugs if coditions aren't met
-const getFriendName = ({user, room}) => {
-    let friendName = user.friends.filter(aFriend => {
-        return aFriend.roomID === room.roomID
-    }); 
-    return friendName[0];
-};
-
-const getFriendLocation = () => {
-
-};
+import {useContext} from "react";
+import GlobalContext from "../GlobalContext";
 
 const Friends = ({roomID, name}) => {
+
+    const {setRoom} = useContext(GlobalContext);
+
+    const handleFriend = ({socket, user}) => {
+    socket.emit('join-room', {_id: user._id, name: user.name, 
+    inUkraine: user.inUkraine}); 
+    };
+
+    const handleSwitch = async({socket, aFriend, user, room, GetRoomData}) => {
+        const ret = await GetRoomData(aFriend.roomID, user.username, user.password);
+        socket.emit('leave-room', room.room);
+        socket.emit('switch-room', ret.room); 
+        setRoom(ret); 
+    };
+
+    //When your calling this function make sure that thre is more than one
+    //friend. --> Could cause bugs if coditions aren't met
+    const getFriendName = ({user, room}) => {
+        let friendName = user.friends.filter(aFriend => {
+            return aFriend.roomID === room.roomID
+        }); 
+        return friendName[0];
+    };
+
+    const getFriendLocation = () => {
+
+    };
+
     return (
         <div className="single-friend">
                 <div className="profile-pic">
