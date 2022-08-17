be4dbe743ace23c2bd1d85c3519b3416e34dd252
veevekdave <veevekd401@gmail.com>
2022-08-17 16:13:51 -0400

    Updated message component with donation amount

--- client/src/VeevekComponents/Message.js
+++ client/src/VeevekComponents/Message.js
@@ -5,8 +5,8 @@ const Message = (props) => {
     const{userID, _id, message, donation, donationAmount} = props;
     return (
         <div className= {userID === _id ? "grey-msg" : "blue-msg"} >
-            <p className={donation && <p>{donationAmount}</p>}></p>
-            <p>I believe in you. </p>
+            <p className= {donation && <p>{donationAmount}</p>}></p>
+            <p>{message}</p>
         </div>
         
     );
