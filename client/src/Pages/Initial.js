b91f6886d9e89f1e0128cf869ce7724428fb8ceb
Tashi Sherpa <thetoshinator@tashis-mbp.myfiosgateway.com>
2022-08-16 22:11:13 -0400

    Finished initial.js

--- client/src/Pages/Initial.js
+++ client/src/Pages/Initial.js
@@ -10,18 +10,20 @@ const Initial = () => {
 
   return (
     <div className="initialPage">
-      <SmsIcon className="logo" sx={{ fontSize: 105 }} />
-      <h1>UChat</h1>
+      <div className="logoTitle">
+        <SmsIcon className="logo" sx={{ fontSize: 120 }} />
+        <h1>UChat</h1>
+      </div>
       <h2>Support your friends in Ukraine</h2>
       <section className="buttonDiv">
         <div className="btnLogin-div">
           <button className="btnLogin" onClick={handleClickLogin}>
-            <p className="loginText">Login</p>
+            <p>Login</p>
           </button>
         </div>
         <div className="btnSignUp-div">
           <button className="btnSignUp" onClick={handleClickSignUp}>
-            <p className="signUpText">Sign Up</p>
+            <p>Sign Up</p>
           </button>
         </div>
       </section>
