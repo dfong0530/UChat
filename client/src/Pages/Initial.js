b31a8e24c5f9f383df3ce02e5e58886b5651fac9
Tashi Sherpa <thetoshinator@tashis-mbp.myfiosgateway.com>
2022-08-17 22:58:24 -0400

    work on donationbox component, no button functionality

--- client/src/Pages/Initial.js
+++ client/src/Pages/Initial.js
@@ -2,32 +2,42 @@ import "./CSS/Initial.css";
 import { useState, useEffect, useContext } from "react";
 import React from "react";
 import SmsIcon from "@mui/icons-material/Sms";
+import DonationBox from "../TashiComponents/DonationBox";
+import GlobalContext from "../GlobalContext";
 
-const Initial = () => {
-  const handleClickLogin = () => {};
+const Initial = () => { 
 
-  const handleClickSignUp = () => {};
+  const {navigate} = useContext(GlobalContext);
+
+  const handleClickLogin = () => {
+    navigate("/login");
+  };
+
+  const handleClickSignUp = () => {
+    navigate("/create-account");
+  };
 
   return (
-    <div className="initialPage">
-      <div className="logoTitle">
-        <SmsIcon className="logo" sx={{ fontSize: 120 }} />    eefe
-        <h1>UChat</h1>
-      </div>
-      <h2>Support your friends in Ukraine</h2>
-      <section className="buttonDiv">
-        <div className="btnLogin-div">
-          <button className="btnLogin" onClick={handleClickLogin}>
-            <p>Login</p>
-          </button>
-        </div>
-        <div className="btnSignUp-div">
-          <button className="btnSignUp" onClick={handleClickSignUp}>
-            <p>Sign Up</p>
-          </button>
-        </div>
-      </section>
-    </div>
+    <DonationBox />
+    // <div className="initialPage">
+    //   <div className="logoTitle">
+    //     <SmsIcon className="logoUChat" sx={{ fontSize: 120 }}/>
+    //     <h1 className="initialTitle">UChat</h1>
+    //   </div>
+    //   <h2 className="initialHeading">Support your friends in Ukraine</h2>
+    //   <section className="buttonDiv">
+    //     <div className="btnLogin-div">
+    //       <button className="btnLogin" onClick={handleClickLogin}>
+    //         <p>Login</p>
+    //       </button>
+    //     </div>
+    //     <div className="btnSignUp-div">
+    //       <button className="btnSignUp" onClick={handleClickSignUp}>
+    //         <p>Sign Up</p>
+    //       </button>
+    //     </div>
+    //   </section>
+    // </div>
   );
 };
 
