a8acb992f071c0d96e8169a6f14416f414e14de4
veevekdave <veevekd401@gmail.com>
2022-08-24 19:01:07 -0400

    styled form

--- client/src/Pages/CreateAccount.js
+++ client/src/Pages/CreateAccount.js
@@ -1,98 +1,98 @@
-import { useState, useRef, useContext, useEffect } from "react";
+import { useState, useRef } from "react";
 import "./CSS/CreateAccount.css";
-import {CreateAccountRequest, GetLocation} from "../Data/GetData";
 import SmsIcon from "@mui/icons-material/Sms";
-import GlobalContext from "../GlobalContext";
 
 const CreateAccount = () => {
     const [form, setForm] = useState({
         name:"",
         username: "",
         password: "",
-        location: "",
-        inUkraine: false
+        location: ""
     });
-
     const usernameRef = useRef(null);
-    const {navigate} = useContext(GlobalContext);
+// =======
+// import Message from "../VeevekComponents/Message"
+// import {useState, useEffect, useContext} from "react";
+// import GlobalContext from "../GlobalContext";
+// import { GetLocation, CreateAccountRequest } from "../Data/GetData";
+
+// const CreateAccount = () => {
+
+//     const {} = useContext(GlobalContext); //VEEVEK --> Write any global variables you need here
+//     const [form, setForm] = useState({name: "", username: "", password: "", location: ""});
 
 
-    useEffect(() => {
-        usernameRef.current.focus();
-    }, [])
+//     useEffect(() => {
 
-    useEffect(() => {
+//         // usernameRef.current.focus();
+//         navigator.geolocation.getCurrentPosition(async function(position) {
+//             const location = await GetLocation(position.coords.latitude, position.coords.longitude);
+//             setForm({...form, location: `${location.city}, ${location.country}`});
+//         });
 
-        // usernameRef.current.focus();
-        navigator.geolocation.getCurrentPosition(async function(position) {
-            const location = await GetLocation(position.coords.latitude, position.coords.longitude);
-
-            if(location.country.toLowerCase() === "ukraine"){
-                setForm({...form, inUkraine: true})
-            }
-
-            setForm({...form, location: `${location.city}, ${location.country}`});
-        });
-
-    }, []);
+//     }, []);
 
 
-    const handleSubmit = async(e) =>{
+
+//     return (
+//         <>
+//             <Message />
+//         </>
+//     )
+// }
+
+    const handleSubmit = (e) =>{
         e.preventDefault();
-        
-        //{status: int, user: }
-        const data = await CreateAccountRequest(form);
-
-        if(data.status === 200){
-            navigate('/login');
-        }
-        else{
-            setForm({username: '', password: '', name: '', location: '', inUkraine: false});
-            usernameRef.current.focus();
-        }
+        console.log(form);
     }
 
     return (
         <div className="app">
             <form onSubmit={handleSubmit}>
-            <SmsIcon
-            className="logo"
-            sx={{
-                fontSize: 120,
-                "@media (max-width: 1000px)": { fontSize: 108 },
-                "@media (max-width: 800px)": { fontSize: 96 },
-                "@media (max-width: 600px)": { fontSize: 80 },
-                "@media (max-width: 425px)": { fontSize: 72 },
-            }}
-            />
 
-                <h1>UChat</h1>
-                <input
-                    className="input"
-                    placeholder="Username"
-                    value={form.name}
-                    onChange={(e) => setForm({ ...form, name: e.target.value })}
-                    required
-                    ref={usernameRef}
+                            
+            <div className = "flex">
+                <SmsIcon
+                className="logo"
+                sx={{
+                    fontSize: 120,
+                    "@media (max-width: 1000px)": { fontSize: 108 },
+                    "@media (max-width: 800px)": { fontSize: 96 },
+                    "@media (max-width: 600px)": { fontSize: 80 },
+                    "@media (max-width: 425px)": { fontSize: 72 },
+                }}
                 />
+                    <p className="title">UChat</p>
+                    <input
+                        className="input"
+                        placeholder="Name"
+                        value={form.name}
+                        onChange={(e) => setForm({ ...form, name: e.target.value })}
+                        required
+                        ref={usernameRef}
+                    />
 
-                <input
-                    type="user"
-                    className="input"
-                    placeholder="Username"
-                    value={form.username}
-                    onChange={(e) => setForm({ ...form, username: e.target.value })}
-                    required
-                />
-                <input
-                    type="password"
-                    className="input"
-                    placeholder="Password"
-                    value={form.password}
-                    onChange={(e) => setForm({ ...form, password: e.target.value })}
-                    required
-                />
-                <button>Submit</button>
+                    <input
+                        type="user"
+                        className="input"
+                        placeholder="Username"
+                        value={form.username}
+                        onChange={(e) => setForm({ ...form, username: e.target.value })}
+                        required
+                    />
+
+                    <input
+                        type="password"
+                        className="input"
+                        placeholder="Password"
+                        value={form.password}
+                        onChange={(e) => setForm({ ...form, password: e.target.value })}
+                        required
+                    />
+                    <button>Create Account</button>
+
+                    <p> Already have an account? <a>Login</a></p>
+                </div>
             </form>
         </div>
     )
