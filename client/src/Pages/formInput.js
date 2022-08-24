4a5770155386da9cfef60492192625ecb3ba8764
veevekdave <veevekd401@gmail.com>
2022-08-23 21:40:26 -0400

    Added validation logic

--- client/src/Pages/formInput.js
+++ client/src/Pages/formInput.js
@@ -2,9 +2,13 @@ import "./CSS/formInput.css"
 
 const FormInput = (props) => {
 	const { label, onChange, id, errorMessage, ...inputProps} = props;
+
+	const handleFocus = (e) =>{
+		setFocused(true);
+	};
 	return (
 		<div className="formInput">
-			<input {...inputProps} onChange={onChange}/>
+			<input {...inputProps} onChange={onChange} onBlur{handleFocus} focued = {focused.toString()}/>
 			<span>{errorMessage}</span>
 		</div>
 	)
